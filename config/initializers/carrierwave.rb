cd75d9cedad66eeb01e0278fc04af5b7856bec19
salt00san <missie.sol09@gmail.com>
2020-05-30 12:24:06 +0900

    pull

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: "ap-northeast-1",
   }
 
-  config.fog_directory = "mychatcpace-mm09"
-  config.asset_host = "https://s3-ap-northeast-1.amazonaws.com/mychatcpace-mm09"
+  config.fog_directory = "chatspace2020mikinari"
+  config.asset_host = "https://s3-ap-northeast-1.amazonaws.com/chatspace2020mikinari"
 end
